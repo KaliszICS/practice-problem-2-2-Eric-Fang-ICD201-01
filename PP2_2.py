c12e4832cd693d1a1d8a0c2824823dc7ac319429
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-10-15 14:28:55 +0000

    commit

--- PP2_2.py
+++ PP2_2.py
@@ -1,25 +1,47 @@
-
+'''
+    Lesson: If and Else
+    Author: Eric Fang
+    Date Created: October 15th, 2024
+    Date Last Modified: October 15th, 2024
+'''
 
 def q1(): 
   #Write Assignment code here
+  if (int(input("Input an integer: ")) == 5):
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 
 
 def q2(): 
   #Write Assignment code here
+  if(float(input("Input an integer: ")) > 0):
+    print("Positive")
+  else:
+    print("Negative")
 
 def q3(): 
   #Write Assignment code here
+  if(int(input("Input an integer: ")) % 2 == 0):
+    print("Even")
+  else:
+    print("Odd")
 
 def q4(): 
   #Write Assignment code here
+  if (int(input("Type \"Hello\": ")) == "Hello"):
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
+'''
